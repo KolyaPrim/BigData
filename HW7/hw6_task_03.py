9f8bf37a6897d43ba466bbd58eb2e44f0c7211fb
kolyaprim <kolyaprim@yandex.ru>
2022-04-20 23:41:58 +1000

    Work don't complete

--- HW7/hw6_task_03.py
+++ HW7/hw6_task_03.py
@@ -1,25 +1,46 @@
 from typing import List
 import numpy as np
 
-a = [['-', '-', 'o'],
+a = [['o', '-', 'o'],
      ['-', 'x', 'o'],
-     ['x', 'o', 'x']]
+     ['x', 'x', 'x']]
 
 
 def tic_tac_toe_checker(board: List[List]) -> str:
     # pass
     row, column = np.shape(board)
+    unfinished = False
+    rows = {}
+    columns = {}
+    diagonals = {0: board[0][0], 1: board[row - 1][0]}
+
     for i in range(0, row):
-        for j in range(0, column):
-            pass
-            board[i][j]  # row
+        rows[i] = True
+        columns[i] = True
+        for j in range(0, column - 1):
+            if board[i][j] != board[i][j + 1]:  # row
+                rows[i] = False
+            if board[j][i] != board[j + 1][i]:  # column
+                columns[i] = False
 
-            board[i][j]  # column
+        if board[i][i] != diagonals[0]:  # straight diagonal
+            diagonals[0] = False
 
-        board[i][i]  # straight diagonal
-
-        board[row-1][i] # reverse diagonal
+        if board[row - 1][i] != diagonals[1]:  # reverse diagonal
+            diagonals[1] = False
         row -= 1
 
+    for line in board:
+        if line.__contains__('-'):
+            unfinished = True
+            break
+    if (str(rows.values()).__contains__('True')):
+        symbol = board[rows.keys()[rows.values().index(True)], 0]
+        return f"{symbol} wins"
+    print(rows)
+    print(columns)
+    print(diagonals)
+    print(unfinished)
 
-tic_tac_toe_checker(a)
+
+print(tic_tac_toe_checker(a))
